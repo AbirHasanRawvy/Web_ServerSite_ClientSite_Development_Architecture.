49ecf6a8c9b80dfec8079d013843361633609f2a
Abir Hasan <abirhasanrawvy@gmail.com>
2026-07-24 20:44:57 +0600

    Add files via upload

---
+++ class2.js
@@ -0,0 +1,21 @@
+//This all are class1 topic: Class1 was theoritical topic;
+
+//Server-side vs. Client-side | Request-response model | Routing and controller concept | 
+// Middleware concept | Introduction to JSON | Understanding JSON structure | 
+// Best practices of JSON | Understanding HTTP client | HTTP request segments | 
+// Comparison between GET and POST | When to use GET and when to use POST | 
+// Request rate limiting | Response segments | Response status code | 
+// URI naming best practices | HTTP cookies in detail | Uses of HTTP cookies | 
+// Response best practices | Request best practices | Controller best practices | 
+// Middleware best practices
+
+//This all are class2 topic: Implemented topic;
+
+//Server-side vs. Client-side | Request-response model | Routing and controller concept | 
+// Middleware concept | Introduction to JSON | Understanding JSON structure | 
+// Best practices of JSON | Understanding HTTP client | HTTP request segments | 
+// Comparison between GET and POST | When to use GET and when to use POST | 
+// Request rate limiting | Response segments | Response status code | 
+// URI naming best practices | HTTP cookies in detail | Uses of HTTP cookies | 
+// Response best practices | Request best practices | Controller best practices | 
+// Middleware best practices
